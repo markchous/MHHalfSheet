22b7394de7cf9bfe8bc461f245c08e6debecffee
Mark Houston <mark.houston@glooko.com>
2022-04-22 16:20:46 -0400

    Add in-code documentation

--- Sources/MHHalfSheet/HalfSheetStyle.swift
+++ Sources/MHHalfSheet/HalfSheetStyle.swift
@@ -7,19 +7,64 @@
 
 import SwiftUI
 
+/**
+ `HalfSheetStyle` allows you to customize the style of your half sheet modal. All properties are optional. If you don't want to change from the default values, do *not* set the property in your custom style.
+ */
 public protocol HalfSheetStyle {
+    /**
+     `overlayColor` will change the color of the background overlay.
+     */
     var overlayColor: Color { get }
+    
+    /**
+     `backgroundColor` will change the background of the half modal sheet.
+     */
     var backgroundColor: Color { get }
+    
+    /**
+     `closeImageColor` will change the foreground color of the close image button.
+     */
     var closeImageColor: Color { get }
+    
+    /**
+     `disableDragDismiss` will disable the drag to dismiss ability if set to true.
+     */
     var disableDragDismiss: Bool { get }
+    
+    /**
+     `disableCardView` will remove the `CardView` styling from the half sheet modal.
+     */
     var disableCardView: Bool { get }
+    
+    /**
+     `closeImage` will change the system image of the close button. This only accepts the name of system images.
+     */
     var closeImage: String { get }
+    
+    /**
+     `cornerRadius` will change the `cornerRadius` of the `CardView` if it's enabled.
+     */
     var cornerRadius: CGFloat { get }
+    
+    /**
+     `dragOffset` will change the distance the user needs to drag the half sheet modal to dismiss it.
+     */
     var dragOffset: CGFloat { get }
+    
+    /**
+     `padding` will change the padding within the half sheet modal.
+     */
     var padding: CGFloat { get }
+    
+    /**
+     `opacity` will change the opacity value of the overlay.
+     */
     var opacity: Double { get }
 }
 
+/**
+ This is a default implementation of `HalfStyleSheet`. You can pick and choose which stlying options to customize.
+ */
 public extension HalfSheetStyle {
     var overlayColor: Color {
         .black
